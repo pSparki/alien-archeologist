f6dff15fc0edf0db3aa308d0629e89ee9d1895f0
pSparki <alexvause1qq@gmail.com>
2023-02-27 15:58:30 +0000

    added ifs

--- Alien Archeologist Unity/Assets/Scripts/TestScript.cs
+++ Alien Archeologist Unity/Assets/Scripts/TestScript.cs
@@ -7,11 +7,49 @@ public class TestScript : MonoBehaviour
 
     public string startMessage;
     public string updateMessage;
+    public string message1;
+    public string message2;
+    public string message3;
+    public int messageChoice;
+
+    public string[] messages;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.LogWarning(startMessage);
+        //Debug.Log(startMessage);
+
+        //messageChoice = Random.Range(1, 4);
+
+        /*
+        if (messageChoice == 1)
+        {
+            Debug.Log(messages[0]);
+        
+        }
+        else if (messageChoice == 2)
+        {
+            Debug.Log(messages[1]);
+        }
+        else if (messageChoice == 3)
+        {
+            Debug.Log(messages[2]);
+        }
+        else
+        {
+            Debug.LogWarning("invalid message choice");
+        }
+        */
+
+        if (messageChoice > 0 && messageChoice <= messages.Length - 1) 
+        {
+            Debug.Log(messages[messageChoice - 1]);
+        }
+        else
+        {
+            Debug.LogWarning("invalid message choice");
+        }
+
     }
 
     // Update is called once per frame

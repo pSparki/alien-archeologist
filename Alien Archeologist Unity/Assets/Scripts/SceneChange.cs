d3422b62486d2b357e9dc7b835dd74073d980046
pSparki <alexvause1qq@gmail.com>
2023-03-13 15:59:33 +0000

    Created new scene, created new script

---
+++ Alien Archeologist Unity/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour
+{
+    public void ChangeToScene(string targetSceneName)
+    {
+
+    }
+}

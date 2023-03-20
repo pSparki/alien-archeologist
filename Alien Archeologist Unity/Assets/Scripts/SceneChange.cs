4f2490cf53bbc9c0bd0762a26e1cd75f42b71555
pSparki <alexvause1qq@gmail.com>
2023-03-20 14:35:40 +0000

    SceneChange script made

--- Alien Archeologist Unity/Assets/Scripts/SceneChange.cs
+++ Alien Archeologist Unity/Assets/Scripts/SceneChange.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
     public void ChangeToScene(string targetSceneName)
     {
-
+        SceneManager.LoadScene(targetSceneName);
     }
 }
